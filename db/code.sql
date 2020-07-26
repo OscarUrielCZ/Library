926db5b100322b5b71452e1849eb85e99e2f3021
OscarUrielCZ <oscar2410.oucz@gmail.com>
2020-07-25 20:36:52 -0500

    Diseñé la base de datos, solo falta capturar información

---
+++ db/code.sql
@@ -0,0 +1,36 @@
+create table users (
+	id varchar(15) not null,
+	username varchar(50) not null unique,
+	password varchar(50) not null,
+	usertype varchar(20) not null,
+	primary key (id)
+);
+create table books (
+	id varchar(15) not null,
+	title varchar(50) not null,
+	author varchar(50) not null,
+	desc text,
+	imguri varchar(50),
+	primary key (id)
+);
+create table pms (
+	id varchar(15) not null,
+	title varchar(50) not null,
+	author varchar(50) not null,
+	desc text,
+	imguri varchar(50),
+	checked boolean not null,
+	userid varchar(15) not null,
+	bookid varchar(15) not null,
+	foreign key (userid) references users(id),
+	foreign key (bookid) references books(id),
+	primary key (id, userid, bookid)
+);
+create table libraries (
+	userid varchar(15) not null,
+	bookid varchar(15) not null,
+	progress varchar(15) not null,
+	foreign key (userid) references users(id),
+	foreign key (bookid) references books(id),
+	primary key (userid, bookid)
+);
