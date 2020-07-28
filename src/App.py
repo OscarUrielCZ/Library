f01b87baf22074af36fa524691a58b86f37f0a13
OscarUrielCZ <oscar2410.oucz@gmail.com>
2020-07-28 15:38:58 -0500

    Frame con scroll hecho

--- src/App.py
+++ src/App.py
@@ -39,16 +39,28 @@ class App(tk.Frame):
 		self.__menubar.add_cascade(label="Sesión", menu=self.__session_menu)
 		self.__menubar.add_cascade(label="Mi librería", menu=self.__mylibrary_menu)
 		self.__menubar.add_cascade(label="Ayuda", menu=self.__help_menu)
-		self.__left_frame = tk.Frame(self.__master, width=500, height=600)
-		self.__left_frame.pack(side=tk.LEFT)
-		self.__right_frame = tk.Frame(self.__master, width=500, height=600)
+
+		self.__canvas = tk.Canvas(self.__master, width=350, height=500)
+		self.__left_frame = tk.Frame(self.__canvas)
+		self.__scrollbar = tk.Scrollbar(self.__master, command=self.__canvas.yview)
+		self.__canvas.config(yscrollcommand=self.__scrollbar.set)
+		self.__canvas.pack(side=tk.LEFT, fill=tk.X)
+		self.__scrollbar.pack(side=tk.LEFT, fill=tk.Y)
+		self.__canvas.create_window((1, 1), window=self.__left_frame)
+		self.__left_frame.bind("<Configure>", self.onFrameConfigure)
+
+		self.__right_frame = tk.Frame(self.__master, width=400, height=500)
 		self.__right_frame.pack(side=tk.LEFT)
 
+	def onFrameConfigure(self, event):
+		self.__canvas.configure(scrollregion=self.__canvas.bbox("all"))
+
 	def __load_books(self, books):
-		for book in books:
-			frame = tk.Frame(self.__left_frame)
-			title = tk.Label(frame, text=book[1])
+		finalbooks = books[:70]
+		for book in finalbooks:
+			frame = tk.Frame(self.__left_frame, pady=5)
+			title = tk.Label(frame, text=book[1], font=('Helvetica', 11, 'bold'))
 			author = tk.Label(frame, text=book[2])
 			title.pack()
 			author.pack()
-			frame.pack()
+			frame.pack(fill=tk.X)
